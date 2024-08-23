67435574bc1bb037c489a10d46d78abebaf79906
Michael Ludick <michael.ludick13@outlook.com>
2024-08-23 12:44:59 +0200

    Finshed DB setup

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,15 +1,38 @@
 ﻿namespace WebAPI_1.Data
 {
     using Microsoft.EntityFrameworkCore;
+    //using System.Reflection.Emit;
+    //using System.Xml;
     using WebAPI_1.Models;
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
            : base(options)
         {
-
+            
         }
         public DbSet<UserData> UserData { get; set; }
         public DbSet<UserLoginInfo> UserLoginInfo { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserLoginInfo>()
+           .Property(e => e.Password)
+           .HasColumnType("binary(64)");
+
+            modelBuilder.Entity<UserLoginInfo>()
+            .HasKey(e => e.id); // Set Id as primary key
+
+            modelBuilder.Entity<UserLoginInfo>()
+                .Property(e => e.id)
+                .ValueGeneratedOnAdd(); // Ensure auto-increment
+
+            modelBuilder.Entity<UserData>()
+            .HasKey(e => e.id); // Set Id as primary key
+
+            modelBuilder.Entity<UserData>()
+                .Property(e => e.id)
+                .ValueGeneratedOnAdd(); // Ensure auto-increment
+        }
     }
 }
