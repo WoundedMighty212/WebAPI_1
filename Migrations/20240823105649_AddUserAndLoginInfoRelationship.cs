2af7bfa24d496d613c49435c5f1feef027af9451
Michael Ludick <michael.ludick13@outlook.com>
2024-08-23 12:57:23 +0200

    updated db with new stuff

---
+++ Migrations/20240823105649_AddUserAndLoginInfoRelationship.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAndLoginInfoRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserData_FKID",
+                table: "UserData",
+                column: "FKID",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserData_UserLoginInfo_FKID",
+                table: "UserData",
+                column: "FKID",
+                principalTable: "UserLoginInfo",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserData_UserLoginInfo_FKID",
+                table: "UserData");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserData_FKID",
+                table: "UserData");
+        }
+    }
+}
