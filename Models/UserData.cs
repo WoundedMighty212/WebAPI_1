85bb13c567fe47e1ad39bbe1b30e394193a8eae7
Michael Ludick <michael.ludick13@outlook.com>
2024-08-23 12:16:07 +0200

    Added Models: user data and user login info

---
+++ Models/UserData.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAPI_1.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class UserData
+    {
+        [Key]
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddelName { get; set; }
+        public string LastName { get; set; }
+        public string Birthdate { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string StreetAddress { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public string Sex { get; set; }
+        public string Race { get; set; }
+        public string AccountName { get; set; }
+        public int FKID { get; set; }
+    }
+}
---
+++ Models/UserLoginInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebAPI_1.Models
+{
+    public class UserLoginInfo
+    {
+        [Key]
+        public int id { get; set; }
+        public string AccountName { get; set; }
+        public string EmailAddress { get; set; }
+        public byte[] Password { get; set; }
+    }
+}
